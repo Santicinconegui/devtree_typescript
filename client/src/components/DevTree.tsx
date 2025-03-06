12d196705fe829d844aded72a76d6b325e4440a0
santicinconegui <santiagocinconegui@gmail.com>
2025-03-05 23:39:36 -0300

    se agrego la funcion de drag and drop

--- client/src/components/DevTree.tsx
+++ client/src/components/DevTree.tsx
@@ -1,8 +1,11 @@
 import { Link, Outlet } from 'react-router-dom';
 import { Toaster } from 'sonner';
 import { SocialNetwork, User } from '../types';
-import NavigationTabs from '../components/NavigationTabs';
 import { useEffect, useState } from 'react';
+import { DndContext, DragEndEvent, closestCenter } from '@dnd-kit/core';
+import { SortableContext, verticalListSortingStrategy, arrayMove } from '@dnd-kit/sortable';
+import { useQueryClient } from '@tanstack/react-query';
+import NavigationTabs from '../components/NavigationTabs';
 import DevTreeLink from './DevTreeLink';
 
 type DevTreeProps = {
@@ -16,6 +19,30 @@ export default function DevTree({ data }: DevTreeProps) {
   useEffect(() => {
     setEnabledLinks(JSON.parse(data.links).filter((item: SocialNetwork) => item.enabled));
   }, [data]);
+
+  const queryClient = useQueryClient();
+  const handleDragEnd = (e: DragEndEvent) => {
+    const { active, over } = e;
+
+    if (over && over.id) {
+      const prevIndex = enabledLinks.findIndex((link) => link.id === active.id);
+      const newIndex = enabledLinks.findIndex((link) => link.id === over.id);
+      const order = arrayMove(enabledLinks, prevIndex, newIndex);
+      setEnabledLinks(order);
+
+      const disabledLinks: SocialNetwork[] = JSON.parse(data.links).filter(
+        (item: SocialNetwork) => !item.enabled
+      );
+      const links = order.concat(disabledLinks);
+
+      queryClient.setQueryData(['user'], (prevData: User) => {
+        return {
+          ...prevData,
+          links: JSON.stringify(links),
+        };
+      });
+    }
+  };
 
   return (
     <>
@@ -56,11 +83,16 @@ export default function DevTree({ data }: DevTreeProps) {
               <p className="text-3xl text-center text-white">{data.handle}</p>
               {data.image && <img src={data.image} alt={data.handle} className="mx-auto max-w-[250px]" />}
               <p className="text-center text-lg font-black text-white">{data.description}</p>
-              <div className="mt-20 flex flex-col gap-5">
-                {enabledLinks.map((link) => (
-                  <DevTreeLink key={link.name} link={link} />
-                ))}
-              </div>
+
+              <DndContext collisionDetection={closestCenter} onDragEnd={handleDragEnd}>
+                <div className="mt-20 flex flex-col gap-5">
+                  <SortableContext items={enabledLinks} strategy={verticalListSortingStrategy}>
+                    {enabledLinks.map((link) => (
+                      <DevTreeLink key={link.name} link={link} />
+                    ))}
+                  </SortableContext>
+                </div>
+              </DndContext>
             </div>
           </div>
         </main>
